6b4d0b9a0d34726bcbf4d3debc5415ca4d82f1d6
Agus Santoso <a.santoso370@gmail.com>
2025-04-29 02:43:27 +0700

    refactor: tambahkan error_reporting di formulir

--- siswa/formulir.php
+++ siswa/formulir.php
@@ -1,6 +1,6 @@
 <?php 
     session_start();
-    // error_reporting(0);
+    error_reporting(0);
     include '../vendor/autoload.php';
     include '../config/koneksi.php';
     include "../config/fungsi_login.php";
